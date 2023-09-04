052818211e6eb1418239c60a0102aefb8ad9c97a
WEI HANG MOH <2230139@ecc.ac.jp>
2023-09-04 14:00:01 +0900

    Add project files.

---
+++ SourceCode/scene_game.h
@@ -0,0 +1,18 @@
+#ifndef SCENE_GAME_H
+#define SCENE_GAME_H
+
+//******************************************************************************
+//
+//
+//      scene_game.h
+//
+//
+//******************************************************************************
+
+// 関数のプロトタイプ宣言
+void game_init();
+void game_deinit();
+void game_update();
+void game_render();
+
+#endif//SCENE_GAME_H
