49a405574295f97436bdfffe509f66b92ddc22d1
ILTD <heinhtet9512@gmail.com>
2023-09-05 12:21:07 +0900

    meteor_random

--- SourceCode/meteor.cpp
+++ SourceCode/meteor.cpp
@@ -1,4 +1,5 @@
 #include "all.h"
+#include <random>
 
 int meteor_state;
 
@@ -24,7 +25,7 @@ void meteor_update() {
         /*fallthrough*/
     case 1:
         meteor.timer = 0; 
-        meteor.pos = { 1000, 100 };
+        meteor.pos = { meteor_random_spawning(500, 2000), meteor_random_spawning(0, 400)};
         meteor.scale = { 0.3f, 0.3f };
         meteor.texPos = { 0,0 };
         meteor.texSize = { 500, 500 };
@@ -55,4 +56,15 @@ void meteor_render() {
 
 void meteor_move() {
     meteor.pos.x -= 2.0f; 
+}
+
+float meteor_random_spawning(int lower_bound, int upper_bound) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    // Create a distribution that maps the random numbers within the specified range
+    std::uniform_int_distribution<> distribution(lower_bound, upper_bound);
+    // Generate random numbers and print them
+
+    int randomNumber = distribution(gen);
+    return randomNumber;
 }
