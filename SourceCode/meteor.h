ff6066635e495670923dc4fc885949fc4672cbb3
ILTD <heinhtet9512@gmail.com>
2023-09-05 11:14:22 +0900

    meteor update

---
+++ SourceCode/meteor.h
@@ -0,0 +1,13 @@
+#ifndef METEOR_H
+#define METEOR_H
+
+void meteor_init();
+void meteor_deinit();
+void meteor_update();
+void meteor_render();
+
+void meteor_move();
+void meteor_random_spawning();
+void meteor_rotation();
+
+#endif //METEOR_H
